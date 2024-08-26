00dce454f62be8aa57d44bdcef4c32a4c0f15c97
joelans <baileyderek59@gmail.com>
2024-08-26 19:25:38 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+  for(let i = 0; i < str.length; i++) {
+    if(str.charAt[i] == "(" || str.charAt[i] == "[" || str.charAt[i] == "{" || str.charAt[i] == "|") {
+      stack.push(str.charAt[i]);
+    } else if(str.charAt[i] == ")" && stack[stack.length - 1] == "(") {
+      stack.pop();
+    } else if(str.charAt[i] == "]" && stack[stack.length - 1] == "[") {
+      stack.pop();
+    } else if(str.charAt[i] == "}" && stack[stack.length - 1] == "{") {
+      stack.pop();
+    } else if(str.charAt[i] == "|" && stack[stack.length - 1] == "|") {
+      stack.pop();
+    } else stack.push(str.charAt[i]);
+  }
+  return stack.length == 0;
 }
