1574805561948aae1ce74c5f9d0439140094f4d2
joelans <baileyderek59@gmail.com>
2024-08-28 14:55:55 +0300

    feat: solve brackets task another way

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const hm = new Map(bracketsConfig);
+  const hmcount = new Map();
+  for(const key of hm.keys()) {
+    if(!hmcount.has(key)) hmcount.set(key, 1);
+    else hmcount.set(key, hmcount.get(key) + 1);
+  }
   let stack = [];
   for(let i = 0; i < str.length; i++) {
-    if(str.charAt[i] == "(" || str.charAt[i] == "[" || str.charAt[i] == "{" || str.charAt[i] == "|") {
-      stack.push(str.charAt[i]);
-    } else if(str.charAt[i] == ")" && stack[stack.length - 1] == "(") {
+    if(hm.has(str.charAt(i)) && hmcount.get(str.charAt(i)) > 0) {
+      stack.push(str.charAt(i));
+      hmcount.set(str.charAt(i), hmcount.get(str.charAt(i)) - 1);
+    } else if(stack.length != 0 && str.charAt(i) == hm.get(stack[stack.length - 1])) {
       stack.pop();
-    } else if(str.charAt[i] == "]" && stack[stack.length - 1] == "[") {
-      stack.pop();
-    } else if(str.charAt[i] == "}" && stack[stack.length - 1] == "{") {
-      stack.pop();
-    } else if(str.charAt[i] == "|" && stack[stack.length - 1] == "|") {
-      stack.pop();
-    } else stack.push(str.charAt[i]);
+    } else stack.push(str.charAt(i));
   }
   return stack.length == 0;
 }
